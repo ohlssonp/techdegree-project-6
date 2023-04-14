a216f3a7a9c32169c6a16e55107c1ef8646d52b4
Peter Olsson <bpmolsson@gmail.com>
2023-04-14 14:00:50 +0200

    update

--- JS/app.js
+++ JS/app.js
@@ -1,18 +1,19 @@
-// Define the phrases array
-const phrases = [
-    'bad weather',
-    'spring',
-    'coffee is delicius',
-    'i am hungry',
-    'time flies quickly'
-];
-
 // Define the variables needed to keep track of the game state
-let missed = 0;
 const phraseUl = document.querySelector('#phrase ul');
 const qwerty = document.querySelector('#qwerty');
 const startButton = document.querySelector('.btn__reset');
 const overlay = document.querySelector('#overlay');
+let missed = 0;
+
+// Define the phrases array
+const phrases = [
+    'weather',
+    'spring',
+    'coffee ',
+    'hunger',
+    'javascript'
+];
+
 
 // Hide the start screen overlay when the start button is clicked
 startButton.addEventListener('click', () => {
@@ -83,14 +84,59 @@ qwerty.addEventListener('click', (event) => {
 function gameOver(won) {
     const gameResult = document.createElement('div');
     gameResult.className = 'game-over';
+    
+    const resetButtonParent = document.createElement('div');
+    resetButtonParent.className = 'btn__reset_parent';
+    
+    const resetButton = document.createElement('a');
+    resetButton.href = '#';
+    
     if (won) {
-        overlay.className = 'win';
-        gameResult.textContent = 'Congratulations, you won!';
+      overlay.className = 'win';
+      resetButton.textContent = 'Play Again';
+      gameResult.textContent = 'Congratulations, you won!';
     } else {
-        overlay.className = 'lose';
-        gameResult.textContent = `Sorry, you lost. The phrase was: "${phraseArray.join('')}".`;
+      overlay.className = 'lose';
+      resetButton.textContent = 'Try Again';
+      gameResult.textContent = `Sorry, you lost. The phrase was: "${phraseArray.join('')}".`;
     }
+
+    resetButton.className = 'btn__reset';
+    resetButton.addEventListener('click', () => {
+      // Reset the game
+      missed = 0;
+      const oldPhrase = document.querySelector('#phrase ul');
+      oldPhrase.innerHTML = '';
+      const newPhrase = getRandomPhraseAsArray(phrases);
+      addPhraseToDisplay(newPhrase);
+  
+      // Reset the keyboard
+      const oldButtons = document.querySelectorAll('.keyrow button');
+      Array.from(oldButtons).forEach(button => {
+        button.disabled = false;
+        button.classList.remove('chosen');
+      });
+  
+      // Reset the hearts
+      const tries = document.querySelectorAll('.tries');
+      tries.forEach(heart => {
+        heart.firstElementChild.src = 'images/liveHeart.png';
+      });
+  
+      // Hide the overlay
+      overlay.style.display = 'none';
+  
+      // Reset the reset button text
+      document.querySelector('.btn__reset').textContent = 'Start Game';
+    });
+    
+    resetButtonParent.appendChild(resetButton);
+    gameResult.appendChild(resetButtonParent);
     overlay.appendChild(gameResult);
     overlay.style.display = 'flex';
-}
 
+    
+    // Remove the "Start Game" button
+    document.querySelector('.btn__reset').remove();
+  
+}
